d0fbd1840e8708f6b5ea28d18fb56a374f531fce
lunarxlark <lunarxlark@gmail.com>
2023-06-20 15:13:07 +0900

    chore(nvim):add extra lua config

--- home-manager.nix
+++ home-manager.nix
@@ -58,6 +58,46 @@
       colorscheme nordfox
       set clipboard+=unnamedplus
     '';
+    extraLuaConfig = ''
+      vim.opt.termguicolors = true
+      --vim.opt.cmdheight = 0
+      -- #file
+      vim.opt.number = true
+      vim.opt.encoding = "utf-8"           -- vimでの文字エンコーディング
+      vim.opt.fileencodings = "utf-8,sjis" -- バッファの改行コード指定
+      vim.opt.fileformats = "unix,mac,dos" -- バッファの改行コード指定
+      -- #edit
+      vim.opt.showtabline = 0
+      vim.cmd([[noswapfile]])
+      vim.opt.clipboard = "unnamedplus" -- clipboardとの連携
+      --vim.opt.viminfo = "" -- viminfoファイルを作成しない
+      vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
+      vim.opt.undofile = true
+      vim.opt.cursorline = true     -- カーソル位置(行)の非表示
+      vim.opt.virtualedit = "block" -- visual-block時、行末を超えて選択可能にする
+      --opt.fixendofline = false
+      --opt.eol = false -- ファイル末尾にeolを付けない
+      vim.opt.foldmethod = "marker"
+      vim.opt.write = true
+      vim.cmd([[set nowrap]])
+      -- #tab
+      vim.opt.smarttab = true       -- 行頭の余白内で<Tab>を入力すると、'shiftwidth'分をインデントする
+      vim.opt.autoindent = true     -- 自動インデント
+      vim.opt.smartindent = true    -- 自動インデント(ブロック対応)
+      vim.opt.tabstop = 2           -- ファイル内の<Tab>が対応する空白の数
+      vim.opt.softtabstop = 2       -- 編集で<Tab>の幅として使用する空白の数
+      vim.opt.expandtab = true      -- 挿入モードで<Tab>入力時、代わりに使う空白の数
+      vim.opt.shiftwidth = 2        -- 自動インデントでのインデントの長さ
+      -- #search
+      vim.opt.showmatch = true      -- 対応する括弧をハイライトする
+      vim.opt.hlsearch = true       -- 検索結果をハイライト
+      vim.opt.ignorecase = true     -- 大文字/小文字の区別しない
+      vim.opt.smartcase = true      -- 大文字で検索されたら大文字/小文字を区別する
+      -- #json
+      vim.opt.conceallevel = 0      -- ダブルクォーテーションを表示
+      
+      vim.opt.iskeyword:remove({"_"}) -- '-'を単語の区切りにしない
+    '';
   };
 
   programs.zsh = {
