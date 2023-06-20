294b1c65953ff7da45d047e48800bb13bc9be87b
lunarxlark <lunarxlark@gmail.com>
2023-06-20 15:12:31 +0900

    chore(starship):add format any lang

--- home-manager.nix
+++ home-manager.nix
@@ -117,6 +117,63 @@
 
   programs.starship = {
     enable = true;
+    settings = {
+      add_newline = false;
+      character = {
+        success_symbol = "[▶](bold red)[▶](bold yellow)[▶](bold cyan)";
+      };
+      directory = {
+        truncation_length = 1;
+        truncate_to_repo = true;
+        style = "fg:#8ec07c";
+      };
+      git_branch = {
+        format = ''\[[$symbol$branch]($style)\]'';
+        style = "fg:#fabd2f";
+      };
+      git_status = {
+        format = ''([\[$all_status$ahead_behind\]]($style))'';
+        style = "fg:#fb4934";
+      };
+      aws = {
+        format = ''\[[$symbol($profile)]($style)\]'';
+        style = "fg:#fe8019";
+        symbol = " ";
+      };
+      golang = {
+        format = ''\[[$symbol($version)]($style)\]'';
+        style = "fg:#83a598";
+        symbol = " ";
+      };
+      gradle = {
+        format = ''\[[$symbol($version)]($style)\]'';
+        version_format = "v$raw";
+        symbol = "🅶";
+        detect_folders = ["gradle"];
+        style = "bold bright-cyan";
+      };
+      java = {
+        format = ''\[[$symbol($version)]($style)\]'';
+      };
+      kotlin = {
+        format = ''\[[$symbol($version)]($style)\]'';
+      };
+      nodejs = {
+        format = ''\[[$symbol($version)]($style)\]'';
+      };
+      python = {
+        format = ''\[[$symbol$pyenv_prefix($version)(\($virtualenv\))]($style)\]'';
+        symbol = " ";
+      };
+      rust = {
+        format = ''\[[$symbol($version)]($style)\]'';
+        symbol = " ";
+      };
+      terraform = {
+        format = ''\[[$symbol$workspace]($style)\]'';
+        detect_files = [".terraform-version"];
+      };
+    };
   };
 
   programs.wezterm = {
