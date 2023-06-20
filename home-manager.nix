2bd51db86c300be6c4c84b154374175879c61495
lunarxlark <lunarxlark@gmail.com>
2023-06-20 11:27:46 +0900

    chore(wezterm):add extra config

--- home-manager.nix
+++ home-manager.nix
@@ -105,12 +105,30 @@
 
   programs.wezterm = {
     enable = true;
-    #colorSchemes = "nordfox";
     extraConfig = ''
-      local config = {}
-      config.color_scheme = "nordfox"
-      config.font_size = 10
-      return config
+      return {
+        color_scheme = "nordfox",
+        font_size = 10,
+	disable_default_key_bindings = true,
+	leader = { key = "t", mods = "CTRL", timeout_milliseconds = 1000 },
+	keys = {
+	  -- font size
+  	  { key = "-", mods = "SUPER", action = wezterm.action.DecreaseFontSize, },
+	  { key = "=", mods = "SUPER", action = wezterm.action.IncreaseFontSize, },
+	  -- tab
+	  { key = "t", mods = "SUPER", action = wezterm.action.SpawnTab("CurrentPaneDomain"), },
+	  { key = "phys:Tab", mods = "CTRL", action = wezterm.action.ActivateTabRelative(1), },
+	  { key = "phys:Tab", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1), },
+	  -- pane split
+	  { key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }), },
+	  { key = "|", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }), },
+	  -- pane move
+	  { key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left"), },
+	  { key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down"), },
+	  { key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up"), },
+	  { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right"), },
+	}
+      }
     '';
   };
 
