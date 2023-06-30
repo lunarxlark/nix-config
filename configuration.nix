6d9db87dc96886bb5517ca00eaabe97b1fd6f296
lunarxlark <lunarxlark@gmail.com>
2023-06-30 20:39:44 +0900

    chore(configuration.nix):grub configuration limit is 20

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,7 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/nvme0n1";
   boot.loader.grub.useOSProber = true;
+  boot.loader.grub.configurationLimit = 20;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
